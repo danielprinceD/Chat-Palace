c9882175def0e08ef66fb6a9c29a4629b364ed7c
daniel <d.danielprince2003@gmail.com>
2024-04-29 06:30:17 +0530

    feat:Socket Connection Disconnection Setup Completed

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const path = require("path");
 const dotenv = require("dotenv");
+const http = require("http");
+const socketio = require("socket.io");
 const static_loc = path.join(__dirname, "static");
-const home = path.join(static_loc, "index.html");
 const express = require("express");
 dotenv.config();
 console.log("Static File : ", static_loc);
 const port = process.env.PORT || 5000;
 const app = express();
-app.use(express.static(home));
+const Server = http.createServer(app);
+const io = socketio(Server);
 
-app.listen(port, () => {
+io.on("connection", (sock) => {
+  console.log("New User");
+  sock.on("disconnect", () => {
+    console.log("User Disconnected....!");
+  });
+});
+
+app.use(express.static(static_loc));
+Server.listen(port, () => {
   console.log("Server is Running...!");
 });
